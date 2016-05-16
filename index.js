0d82d84d0b918e031a3316b482af9d9c4ea8aeba
Zarema <Zmoki@users.noreply.github.com>
2016-05-16 18:42:12 +0300

    Add resize

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+var widget = uploadcare.Widget('[role=uploadcare-uploader]');
 
-JFCustomWidget.subscribe("ready", function(){
+function resize() {
+    JFCustomWidget.requestFrameResize({
+        width: 618,
+        height: 500
+    });
+}
+
+JFCustomWidget.subscribe('ready', function(){
+    widget.onDialogOpen(function(){
+        resize();
+    });
+    
     uploadcare.start({
       publicKey: 'demopublickey'
     });
