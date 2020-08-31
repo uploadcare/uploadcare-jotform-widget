b578c8d301b25623fc5bd2a90b3d5ae41c7c6d3b
Alex Chernenko <optlsnd@gmail.com>
2020-08-31 14:22:49 +0300

    Fix default empty file name
    If a user sets to add a file name to the output URL and after sanitizing all characters are removed, the "noroot" name leads to downloading the file with the original name, which may cause issues on the user's system. Using the "file" name instead will lead to downloading the file with the name "file.[ext]".

--- widget/index.js
+++ widget/index.js
@@ -37,7 +37,7 @@ function sanitizeFileName(fileName) {
   var name = fileName.substring(0, fileName.length - extension.length)
   name = name.replace(regexp, '')
   if (!name.length) {
-  	return 'noroot' + '.' + extension
+  	return 'file' + '.' + extension
   }
   return name + '.' + extension
 }
